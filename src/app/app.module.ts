591520869a59626ce566b1b545c9033eb18a4683
prithwirajpm <wt.prithwiraj@gmail.com>
2023-12-18 21:34:03 +0530

    Intial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AllProductsComponent,
+    ViewProductComponent,
+    LoginComponent,
+    RegisterComponent,
+    WishlistComponent,
+    CartComponent,
+    CheckoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
